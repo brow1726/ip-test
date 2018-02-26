3ed9a03802249aa2b5fda493275fe962fcefc520
Matthew Brown <mateojos3@gmail.com>
2018-02-26 10:31:06 -0600

    port 443

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('hitting this');
   res.render('index', { title: 'Express' });
 });
 
